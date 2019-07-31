3761d9e5cbc16bb3ed552926753f15011b0c1662
Mr <mickaelmendes50@gmail.com>
2019-07-31 18:08:38 -0300

    conversor: User ViewHolder
    Signed-off-by: Mr <mickaelmendes50@gmail.com>

--- java/com/mesquita/conversormoedas/MainActivity.java
+++ java/com/mesquita/conversormoedas/MainActivity.java
@@ -2,16 +2,29 @@ package com.mesquita.conversormoedas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHoler = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText valor = findViewById(R.id.edit_value);
-        valor.getText();
+        this.mViewHoler.editValue = findViewById(R.id.edit_value);
+        this.mViewHoler.textDolar = findViewById(R.id.text_dolar);
+        this.mViewHoler.textEuro = findViewById(R.id.text_euro);
+        this.mViewHoler.buttonCalculate = findViewById(R.id.button_calculate);
+    }
+
+    private static class ViewHolder {
+        EditText editValue;
+        TextView textDolar;
+        TextView textEuro;
+        Button buttonCalculate;
     }
 }
