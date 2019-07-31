82a48e0b2bf1cc29ab146a63867125917a1c1967
Mr <mickaelmendes50@gmail.com>
2019-07-31 19:45:39 -0300

    button: Add toast notification when value is empty
    Signed-off-by: Mr <mickaelmendes50@gmail.com>

--- java/com/mesquita/conversormoedas/MainActivity.java
+++ java/com/mesquita/conversormoedas/MainActivity.java
@@ -2,11 +2,13 @@ package com.mesquita.conversormoedas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ViewHolder mViewHoler = new ViewHolder();
 
@@ -19,6 +21,26 @@ public class MainActivity extends AppCompatActivity {
         this.mViewHoler.textDolar = findViewById(R.id.text_dolar);
         this.mViewHoler.textEuro = findViewById(R.id.text_euro);
         this.mViewHoler.buttonCalculate = findViewById(R.id.button_calculate);
+
+        this.mViewHoler.buttonCalculate.setOnClickListener(this);
+
+        this.clearValues();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button_calculate) {
+            String value = this.mViewHoler.editValue.getText().toString();
+            if ("".equals(value)) {
+                Toast.makeText(this, this.getString(R.string.informe_valor), Toast.LENGTH_LONG).show();
+            } else {
+
+            }
+        }
+    }
+    private void clearValues() {
+        this.mViewHoler.textDolar.setText("");
+        this.mViewHoler.textEuro.setText("");
     }
 
     private static class ViewHolder {
